6b98bb807198ad80d9c61b291265f71853221e01
Samuel Trahan <samtrahan91@gmail.com>
2021-02-22 21:10:46 -0600

    some nav bar styling

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -6,9 +6,10 @@ export default function NavBar() {
         <div className="navbar">
             <div className="stock-logo">
                 <img className="image" src="/images/stock.png" alt=""></img>
+                <h1 className="title">Cinque Stocks</h1>
             </div>
             <div className="navbar-links">
-                <a href="https://www.cinquewd.com" target='_blank' rel="noreferrer">Cinque</a>
+                <a className="links" href="https://www.cinquewd.com" target='_blank' rel="noreferrer">Cinque</a>
             </div>
         </div>
     )
