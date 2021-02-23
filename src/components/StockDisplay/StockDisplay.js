ca9acefbf217ddacd88ef38ec833f91195a22677
Samuel Trahan <samtrahan91@gmail.com>
2021-02-22 21:02:17 -0600

    displaying logos and information properly

---
+++ src/components/StockDisplay/StockDisplay.js
@@ -0,0 +1,79 @@
+import React, {useState, useEffect} from 'react';
+import './StockDisplay.css';
+import Apple from '../../assets/appleLogo.png'
+import MicroSoft from '../../assets/microsoftLogo.jpeg';
+import FaceBook from '../../assets/facebookLogo.png';
+import Google from '../../assets/googleLogo.png';
+import Tesla from '../../assets/teslaLogo.png';
+import Zoom from '../../assets/zoomLogo.jpg';
+import Zom from '../../assets/zomLogo.png';
+
+
+export default function StockDisplay({stock}) {
+    const [icon, setIcon] = useState('')
+
+    useEffect(() =>  {
+        console.log(stock.symbol)
+        setIcon(stock.symbol)
+    }, [])
+
+    let stockIcon = icon === "AAPL" ? Apple : icon === "MSFT" ? MicroSoft : icon === "FB" ? FaceBook : icon === "GOOG" ? Google : icon === "TSLA" ? Tesla : icon === "ZM" ? Zoom : icon === "ZOM" ? Zom : ''
+
+
+    return (
+        !stock.open || !stock.close || !stock.volume ?
+        <div className="stock-card">
+            <div>
+            {stock.symbol} <br /><br />
+            <img className="stock-image" src={stockIcon} alt=""></img>
+            <h3>Date: {stock.date.slice(0, 10)}</h3>
+            </div>
+            <div>
+                <h4 hidden>Open: ${stock.open}</h4>
+                <h4>High: ${stock.high}</h4>
+                <h4 hidden>Close: ${stock.close}</h4>
+                <h4 hidden>Volume: {stock.volume}</h4>
+            </div>
+        </div> :
+        <div className="stock-card">
+            <div>
+            {stock.symbol} <br /><br />
+            <img className="stock-image" src={stockIcon} alt=""></img>
+            <h3>Date: {stock.date.slice(0, 10)}</h3>
+            </div>
+            <div>
+                <h4>Open: ${stock.open}</h4>
+                <h4>High: ${stock.high}</h4>
+                <h4>Close: ${stock.close}</h4>
+                <h4>Volume: {stock.volume}</h4>
+            </div>
+        </div>
+    //     ? !stock.close || !stock.high || !stock.volume ?
+    //     <div className="stock-card">
+    //     <div>
+    //     {stock.symbol} <br /><br />
+    //     <img className="stock-image" src={stockIcon} alt=""></img>
+    //     <h3>Date: {stock.date.slice(0, 10)}</h3>
+    //     </div>
+    //     <div>
+    //         <h4>Open: ${stock.open}</h4>
+    //         <h4 hidden>High: ${stock.high}</h4>
+    //         <h4 hidden>Close: ${stock.close}</h4>
+    //         <h4 hidden>Volume: {stock.volume}</h4>
+    //     </div>
+    // </div> :
+    // <div className="stock-card">
+    //     <div>
+    //     {stock.symbol} <br /><br />
+    //     <img className="stock-image" src={stockIcon} alt=""></img>
+    //     <h3>Date: {stock.date.slice(0, 10)}</h3>
+    //     </div>
+    //     <div>
+    //         <h4>Open: ${stock.open}</h4>
+    //         <h4>High: ${stock.high}</h4>
+    //         <h4>Close: ${stock.close}</h4>
+    //         <h4>Volume: {stock.volume}</h4>
+    //     </div>
+    // </div>
+    )
+}
--- src/components/Stocks.js
+++ src/components/Stocks.js
@@ -1,12 +1,7 @@
-import React, {useState, useEffect} from 'react';
+import React from 'react';
+import StockDisplay from './StockDisplay/StockDisplay';
 import './Stocks.css';
-import Apple from '../assets/appleLogo.png';
-import MicroSoft from '../assets/microsoftLogo.png';
-import FaceBook from '../assets/facebookLogo.png';
-import Google from '../assets/googleLogo.png';
-import Tesla from '../assets/teslaLogo.png';
-import Zoom from '../assets/zoomLogo.png';
-import Zom from '../assets/zomLogo.png';
+
 
 export default function Stocks({stocks}) {
     
@@ -17,16 +12,7 @@ export default function Stocks({stocks}) {
           <>
                   {stocks.map((stock) => 
                   <div className="stock-display">
-                    <div className="stock-header">
-                        {stock.symbol}<br/>
-                        Date: {stock.date.slice(0, 10)}<br />
-                    </div>
-                    <div className="stock-info">
-                        High: {stock.high} <br />
-                        Open: {stock.open} <br />
-                        Close: {stock.close} <br />
-                        Volume: {stock.volume}
-                    </div>
+                   <StockDisplay stock={stock}/>
                   </div>
                     
 )}
